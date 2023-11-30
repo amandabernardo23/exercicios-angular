cf58f641572bfd14530bf085042d4b7592506469
amandabernardo23 <amanda.bernardocosta@gmail.com>
2023-11-30 05:09:45 -0300

    ex1

---
+++ src/app/contador-component/contador-components/contador-component.spec.ts
@@ -0,0 +1,26 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContadorComponent } from "./contador-component";
+
+describe('ContadorComponentComponent', () => {
+  let component: ContadorComponent;
+  let fixture: ComponentFixture<ContadorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ContadorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContadorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
