ced2d4842a2a834df7061e6f3dec806deeca9ffc
Joffre <joffre_quintas@hotmail.com>
2023-12-01 17:33:40 -0300

    exercico1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { AppComponentPaiComponent } from "./components/app-component-pai/app-component-pai.component";
+import { ContadorComponentComponent } from "./components/contador-component/contador-component.component";
+import { ListItemsComponent } from "./components/list-items/list-items.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, AppComponentPaiComponent]
+    imports: [CommonModule, RouterOutlet, AppComponentPaiComponent, ContadorComponentComponent, ListItemsComponent]
 })
 export class AppComponent {
   title = 'exercicios-angular';
---
+++ src/app/components/list-items/list-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-list-items',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-items.component.html'
+})
+export class ListItemsComponent {
+
+}
